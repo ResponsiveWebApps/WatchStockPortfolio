249ab68013c920a278e8120d3d0afd1c1e068b73
Don Roche <donrochemedia@gmail.com>
2020-11-05 15:19:22 +0100

    Dev for cpanel

--- stocks/settings.py
+++ stocks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x8hrr!cclw(4936lrw=w4!!fwq2q(7z$6qc+45qyffhao!dgje'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [decadeofdata.com/datamarket]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'stocks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_media")
--- stocks/urls.py
+++ stocks/urls.py
@@ -20,3 +20,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotes.urls')), 
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='templates/home.html'), name='home'),
+    path('', include('quotes.urls')), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
