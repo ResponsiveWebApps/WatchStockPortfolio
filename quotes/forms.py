37255e7cb039c1c7e4abb81a819177b716ed790d
Don Roche <donrochemedia@gmail.com>
2020-11-26 20:52:32 +0100

    Added registration with email confirmation

--- quotes/forms.py
+++ quotes/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Stock
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class StockForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ["ticker"] 
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- quotes/urls.py
+++ quotes/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('about.html', views.about, name="about"),
     path('stock_info.html', views.stock_info, name="stock_info"),
     path('watch_stock.html', views.watch_stock, name="watch_stock"),
-    path('delete/<symbol>', views.delete, name="delete")
+    path('delete/<symbol>', views.delete, name="delete"),
+    path('signup.html', views.signup, name='signup'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.activate, name='activate')
 ]
