b41229898841cb9531dd75b33a4d8c63bbf4eb9c
Don Roche <donrochemedia@gmail.com>
2020-12-02 18:23:36 +0100

    Added user column to stock model

--- quotes/forms.py
+++ quotes/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class StockForm(forms.ModelForm):
     class Meta:
         model = Stock
-        fields = ["ticker"] 
+        fields = ["ticker", "user"] 
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
--- quotes/models.py
+++ quotes/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+# Needed for User
+from django.contrib.auth.models import User
+from django.conf import settings
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 class Stock(models.Model):
     ticker = models.CharField(max_length=10)
-
+    user = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
     def __str__(self):
-        return self.ticker.lower()
+        return self.ticker
+
